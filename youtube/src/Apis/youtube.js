dcac2629bf46b4ba8e5a51dc227b014ed81ed0b1
Yash Govani <dhvyas143@gmail.com>
2021-05-03 17:58:54 +0530

    Update youtube.js

--- youtube/src/Apis/youtube.js
+++ youtube/src/Apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyCq3PiLEp7YOzlu-Yr3oBb0TilXNuaQGjI';
+const KEY = //ADD YOUR KEY HERE
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
